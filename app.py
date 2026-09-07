deace16c7bd3c34660046e3dbf7ed760700bb4a0
Mantra Agarwal <mantraagarwal7@gmail.com>
2026-09-08 00:50:57 +0530

    New file added just for test

--- app.py
+++ app.py
@@ -7,5 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 waitress.serve(app, host='0.0.0.0',port=5000)
